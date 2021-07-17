a983d319750093829a2de29446b066a75fe7e268
David Lascabannes <dlascaba@c1r4s1.42lausanne.ch>
2021-07-17 16:57:52 +0200

    Mapping

---
+++ mapping.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mapping.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlascaba <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/17 16:47:03 by dlascaba          #+#    #+#             */
+/*   Updated: 2021/07/17 16:56:28 by dlascaba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	map_colup(char tab[4][4], int cal, char expet_visible);
+int map_coldown(char tab[4][4], int cal, char expet_visible);
+int map_rowleft(char row[4], char expet_visible);
+int map_rowright(char row[4], char expet_visible);
+int map_unique_box(char tab[4][4], int ct_row, int cur_col, char box);
+
+
+int valid_tab(char tab[4][4], char map [16])
+{
+	int	i;
+
+	i
+}
